23311368c8ebe2cf3362be6c7271bf723485f021
ChipTheGreat <lilpal272@gmail.com>
2025-07-31 14:18:55 +0000

    Add files via upload
    now YOU CAN MOVE

--- Gameapp/main/GamePanel.java
+++ Gameapp/main/GamePanel.java
@@ -19,14 +19,22 @@ public class GamePanel extends JPanel implements Runnable{
 	final int ScreenHeight = finalTileSize * amountOfVerticalTiles;
 	
 	Thread gameThread;
+	keyHandler keyH = new keyHandler();
+	
+	int playerx = 100;
+	int playery = 100;
+	double playerSpeed = 4;
 
-
+	//FPS
+	int FPS = 60;
 
 	public GamePanel() {
 		this.setPreferredSize(new Dimension(ScreenWidth, ScreenHeight));
 		this.setBackground(Color.black);
 		this.setDoubleBuffered(true);	
 	
+		this.addKeyListener(keyH);
+		this.setFocusable(true);
 	}
 	public void startGameThread(){
 		gameThread = new Thread(this);
@@ -34,16 +42,43 @@ public class GamePanel extends JPanel implements Runnable{
 	}
 	@Override	
 	public void run(){
+		double drawInterval = 1000000000/FPS;
+		double delta = 0;
+		long lastTime = System.nanoTime();
+		long currentTime;
+
 		while(gameThread != null ){
 			//TODO 
 			// UPDATE THE GAMES INFORMATIONS ETC
-			update();
-			// DRAW THE SCREEN WITH NEW DATA OR UPDATE THE OLD ONES
-			repaint();
-			}			
+
+			currentTime = System.nanoTime();
+			
+			delta += (currentTime - lastTime) / drawInterval;
+			lastTime = currentTime;
+			
+			//just timer so the game isnt super fast
+			if(delta >= 1){
+
+				update();
+				// DRAW THE SCREEN WITH NEW DATA OR UPDATE THE OLD ONES
+				repaint();
+
+				delta--;
+
+			}
+		}			
 	} 
 	public void update(){
 		//Update infos in this methode	
+		if(keyH.upPressed){
+			playery -= playerSpeed; 
+		} else if(keyH.downPressed){
+			playery += playerSpeed;
+		} else if(keyH.rightPressed){
+			playerx += playerSpeed;
+		} else if(keyH.leftPressed){
+			playerx -= playerSpeed;
+		}
 	}
 	public void paintComponent(Graphics g){
 		//Draw objects and sprites here
@@ -51,7 +86,7 @@ public class GamePanel extends JPanel implements Runnable{
 		Graphics gx2d = (Graphics2D)g;
 
 		gx2d.setColor(Color.white);
-		gx2d.fillRect(100, 100, finalTileSize, finalTileSize);
+		gx2d.fillRect(playerx, playery, finalTileSize, finalTileSize);
 
 		gx2d.dispose();
 	}
